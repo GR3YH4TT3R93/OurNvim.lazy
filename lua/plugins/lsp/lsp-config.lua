8f47bd368ab7dc8fe440cab7e42768e11626de29
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-01-12 05:53:07 -0800

    Fix: Fixed Eslint

--- lua/plugins/lsp/lsp-config.lua
+++ lua/plugins/lsp/lsp-config.lua
@@ -33,14 +33,14 @@ return{
     --     capabilities: capabilities,
     --   })
     -- end
-    lspconfig.eslint.setup({
-      on_attach = function(client, bufnr)
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          buffer = bufnr,
-          command = "EslintFixAll",
-        })
-      end,
-    })
+    -- lspconfig.eslint.setup({
+    --   on_attach = function(client, bufnr)
+    --     vim.api.nvim_create_autocmd("BufWritePre", {
+    --       buffer = bufnr,
+    --       command = "EslintFixAll",
+    --     })
+    --   end,
+    -- })
 
     -- lspconfig.pyright.setup({
     --   on_attach = on_attach,
