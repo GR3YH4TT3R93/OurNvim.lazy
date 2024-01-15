48b908d936e7111e074ad03970aa3ba03fa322ad
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-01-14 23:11:01 -0800

    Feat: Replaced Commentary with Comment.nvim

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,43 +4,50 @@ return {
   dependencies = {
     'nvim-treesitter/nvim-treesitter-textobjects',
     'windwp/nvim-ts-autotag',
+    -- 'nvim-treesitter/nvim-treesitter-context',
     'JoosepAlviste/nvim-ts-context-commentstring',
-    -- 'lukas-reineke/indent-blankline.nvim',
-    'tpope/vim-commentary',
+    -- 'tpope/vim-commentary',
+    'numToStr/Comment.nvim',
+    -- opts = {
+    --     -- add any options here
+    -- },
+    -- lazy = false,
   },
   config = function ()
     local configs = require("nvim-treesitter.configs")
+    require('Comment').setup()
     configs.setup({
       auto_install = true,
       sync_install = false,
       ignore_install = {},
-      modules = {}, -- Remove the trailing comma here
       highlight = { enable = true },
       indent = { enable = true },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = '<c-space>',
-          node_incremental = '<c-space>',
-          scope_incremental = '<c-s>',
-          node_decremental = '<M-space>',
-        },
-      },
-      textobjects = {
-        select = {
+      modules = {},
+        incremental_selection = {
           enable = true,
-          lookahead = true,
           keymaps = {
-            ['aa'] = '@parameter.outer',
-            ['ia'] = '@parameter.inner',
-            ['af'] = '@function.outer',
-            ['if'] = '@function.inner',
-            ['ac'] = '@class.outer',
-            ['ic'] = '@class.inner',
+            init_selection = '<leader>ss',
+            node_incremental = '<leader>nn',
+            node_decremental = '<leader>np',
+            scope_incremental = '<leader>sn',
+            scope_dencremental = '<leader>sp',
+          },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
           },
         },
-      },
-    })
+      }
+    }) -- Remove the trailing comma here
   end,
   build = ':TSUpdate',
 }
