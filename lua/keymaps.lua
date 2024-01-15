0cb91f4816b0e9b9a3fa3315ccf61aa5adf88381
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-01-14 23:16:55 -0800

    Feat: Add Keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,4 +1,8 @@
+-- Locals {{{
 local opts = { noremap = true, silent = true }
+local builtin = require("telescope.builtin")
+local extensions = require("telescope").extensions
+--}}}
 
 -- Move Commands{{{
 
@@ -25,6 +29,9 @@ vim.keymap.set('n', '<C-n>', ':Neotree toggle<cr>', opts) -- }}}
 
 -- LazyGit Commands{{{
 vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', opts)
+--}}}
+
+-- Write Quit eXit {{{
 vim.keymap.set('n', '<C-w>', ':w<cr>', opts)
 vim.keymap.set('v', '<C-w>', '<esc> :w<cr>', opts)
 vim.keymap.set('i', '<C-w>', '<esc> :w<cr>', opts)
@@ -33,20 +40,31 @@ vim.keymap.set('v', '<C-q>', '<esc> :q!<cr>', opts)
 vim.keymap.set('i', '<C-q>', '<esc> :q!<cr>', opts)
 vim.keymap.set('n', '<C-x>', ':x<cr>', opts)
 vim.keymap.set('v', '<C-x>', '<esc> :x<cr>', opts)
-vim.keymap.set('i', '<C-x>', '<esc> :x<cr>', opts) -- }}}
+vim.keymap.set('i', '<C-x>', '<esc> :x<cr>', opts)
+--}]}
 
 -- ToggleTerm Commands{{{
 vim.keymap.set('n', '<leader>d', ':TermExec cmd="prd"<CR>')
-vim.keymap.set('i', '<leader>d', ':TermExec cmd="prd"<CR>i') -- }}}
+vim.keymap.set('i', '<leader>d', ':TermExec cmd="prd"<CR>i')
+-- }}}
 
 -- Telescope Commands{{{
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>fr', '<cmd>Telescope lazygit<CR>')
-vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>')
-vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>')
-vim.keymap.set('n', '<leader>fy', '<cmd>Telescope neoclip<CR>')
-vim.keymap.set('n', '<leader>fk', '<cmd>Telescope keymaps<CR>') -- }}}
+vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fgf", builtin.git_files, {})
+vim.keymap.set("n", "<leader>fgs", builtin.git_stash, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+vim.keymap.set("n", "<leader>fd", builtin.lsp_definitions, {})
+vim.keymap.set("n", "<leader>fdt", builtin.lsp_type_definitions, {})
+vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+vim.keymap.set("n", "<leader>fdi", builtin.diagnostics, {})
+vim.keymap.set("n", "<leader>fqf", builtin.quickfix, {})
+vim.keymap.set("n", "<leader>fqfh", builtin.quickfixhistory, {})
+vim.keymap.set("n", "<leader>fi", builtin.lsp_implementations, {})
+vim.keymap.set('n', '<leader>fy', extensions.neoclip.default, {})
+--}}}
 
 -- Nvim Space Folding{{{
 vim.keymap.set('n', '<Space>', "@=(foldlevel('.')?'za':'\\<Space>')<CR>", opts)
@@ -56,7 +74,8 @@ vim.keymap.set('v', '<Space>', 'zf', opts)
 -- Close Current Buffer{{{
 vim.keymap.set('n', '<leader>bd', ':bd<cr>', opts)
 vim.keymap.set('v', '<leader>bd', '<esc> :bd<cr>', opts)
-vim.keymap.set('i', '<leader>bd', '<esc> :bd<cr>', opts) -- }}}
+vim.keymap.set('i', '<leader>bd', '<esc> :bd<cr>', opts)
+-- }}}
 
 -- Move to Previous and Next Buffer {{{
 vim.keymap.set('n', '<C-h>', ':bprev<cr>', opts)
@@ -79,6 +98,10 @@ vim.api.nvim_create_autocmd('BufWritePre', {
   pattern = { '*.tsx', '*.ts', '*.jsx', '*.js', '*.vue' },
   command = 'silent! EslintFixAll',
   group = vim.api.nvim_create_augroup('MyAutocmdsJavaScripFormatting', {}),
+})
+vim.api.nvim_create_autocmd({'InsertLeave','TextChanged'}, {
+  pattern = { '*.tsx', '*.ts', '*.jsx', '*.js', '*.vue' },
+  command = 'silent! EslintFixAll',
 })
 -- }}}
 
