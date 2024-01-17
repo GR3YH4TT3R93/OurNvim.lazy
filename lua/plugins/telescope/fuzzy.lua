83428ee205132c5af0f0a62cd14f69d222c4d8f3
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-01-16 22:03:19 -0800

    Fix: Snippets Regression

--- lua/plugins/telescope/fuzzy.lua
+++ lua/plugins/telescope/fuzzy.lua
@@ -1,5 +1,5 @@
 return {
-  {'tzachar/fuzzy.nvim', requires = {'hrsh7th/nvim-cmp', 'nvim-telescope/telescope-fzf-native.nvim'}},
- {'tzachar/cmp-fuzzy-buffer', requires = {'hrsh7th/nvim-cmp', 'tzachar/fuzzy.nvim'}},
- {'tzachar/cmp-fuzzy-path', requires = {'hrsh7th/nvim-cmp', 'tzachar/fuzzy.nvim'}}
+  {'tzachar/fuzzy.nvim', dependencies = {'hrsh7th/nvim-cmp', 'nvim-telescope/telescope-fzf-native.nvim'}},
+ {'tzachar/cmp-fuzzy-buffer', dependencies = {'hrsh7th/nvim-cmp', 'tzachar/fuzzy.nvim'}},
+ {'tzachar/cmp-fuzzy-path', dependencies = {'hrsh7th/nvim-cmp', 'tzachar/fuzzy.nvim'}}
 }
