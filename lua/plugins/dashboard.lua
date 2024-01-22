fdddf46efb1f0689d3b1b3ef8f6927c20d3ae5d6
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-01-22 04:35:05 -0800

    Chore: Standardize Quotation Marks

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -1,10 +1,10 @@
 return {
-  'nvimdev/dashboard-nvim',
+  "nvimdev/dashboard-nvim",
   config = function()
-    require('dashboard').setup {
-      theme = 'hyper', --  theme is doom and hyper default is hyper
+    require("dashboard").setup {
+      theme = "hyper", --  theme is doom and hyper default is hyper
       disable_move = false,    --  default is false disable move keymap for hyper
-      shortcut_type = 'letter',   --  shorcut type 'letter' or 'number'
+      shortcut_type = "letter",   --  shorcut type "letter" or "number"
       change_to_vcs_root = true, -- default is false,for open file in hyper mru. it will change to the root of vcs
       config = {    --  config used for theme
         header = {
@@ -28,39 +28,39 @@ return {
         },
         shortcut = {
           {
-            icon = '󰊳 ',
-            desc = 'Update Lazy',
-            group = '@property',
-            action = 'require("lazy").update()',
-            key = 'l'
+            icon = "󰊳 ",
+            desc = "Update Lazy",
+            group = "@property",
+            action = "require('lazy').update()",
+            key = "l"
           },
           {
-            icon = '󰊳 ',
-            desc = 'Update Mason',
-            group = 'number',
-            action = 'MasonUpdate',
-            key = 'm'
+            icon = "󰊳 ",
+            desc = "Update Mason",
+            group = "number",
+            action = "MasonUpdate",
+            key = "m"
           },
           {
-            icon = ' ',
-            desc = 'Files',
-            group = 'Label',
-            action = 'Telescope find_files',
-            key = 'f',
+            icon = " ",
+            desc = "Files",
+            group = "Label",
+            action = "Telescope find_files",
+            key = "f",
           },
           {
-            icon = '󰝰 ',
-            desc = 'Neo Tree',
-            group = '@type',
-            action = 'Neotree toggle',
-            key = 'n',
+            icon = "󰝰 ",
+            desc = "Neo Tree",
+            group = "@type",
+            action = "Neotree toggle",
+            key = "n",
           },
         },
       },
       hide = {
         statusline = false,    -- hide statusline default is true
-        tabline,       -- hide the tabline
-        winbar,        -- hide winbar
+        tabline = true,       -- hide the tabline
+        winbar = true,        -- hide winbar
       },
       preview = {
         -- command,       -- preview command
