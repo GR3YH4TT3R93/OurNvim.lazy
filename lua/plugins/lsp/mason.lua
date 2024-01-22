dae35c6f1a4505b239f7d5acdcd6283dee6697f3
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-01-22 04:07:37 -0800

    Feat: Update Lua Globals

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -53,6 +53,9 @@ return {
         if not vim.loop.fs_stat(path.."/.luarc.json") and not vim.loop.fs_stat(path.."/.luarc.jsonc") then
           client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
             Lua = {
+              diagnostics = {
+                globals = { "vim", "bufnr", "tabline", "winbar" }
+              },
               runtime = {
                 -- Tell the language server which version of Lua you're using
                 -- (most likely LuaJIT in the case of Neovim)
