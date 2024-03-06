65f687a27f2102e6fce779b1601a006762974035
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-03-06 06:23:47 -0800

    Feat: Update Mason For Use With Linux

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -76,42 +76,68 @@ return {
         -- ["rust_analyzer"] = function ()
         --     require("rust-tools").setup {}
         -- end
+        ["lua_ls"] = function()
+          require("lspconfig").lua_ls.setup({
+            settings = {
+              Lua = {
+                diagnostics = {
+                  -- globals = { "vim", "bufnr" },
+                },
+                runtime = {
+                  -- Tell the language server which version of Lua you're using
+                  -- (most likely LuaJIT in the case of Neovim)
+                  version = "LuaJIT",
+                },
+                -- Make the server aware of Neovim runtime files
+                workspace = {
+                  checkThirdParty = false,
+                  library = {
+                    vim.env.VIMRUNTIME,
+                    "${3rd}/luv/library",
+                    -- unpack(vim.api.nvim_get_runtime_file("", true)),
+                    -- "${3rd}/busted/library",
+                  },
+                },
+              },
+            },
+          })
+        end,
       },
     })
 
     -- Servers that are not installed by mason.
-    require("lspconfig").lua_ls.setup({
-      on_init = function(client)
-        local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-          client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-            Lua = {
-              diagnostics = {
-                globals = { "vim", "bufnr" },
-              },
-              runtime = {
-                -- Tell the language server which version of Lua you're using
-                -- (most likely LuaJIT in the case of Neovim)
-                version = "LuaJIT",
-              },
-              -- Make the server aware of Neovim runtime files
-              workspace = {
-                checkThirdParty = false,
-                library = {
-                  vim.env.VIMRUNTIME,
-                  -- "${3rd}/luv/library"
-                  -- "${3rd}/busted/library",
-                },
-                -- or pull in all of "runtimepath". NOTE: this is a lot slower
-                -- library = vim.api.nvim_get_runtime_file("", true)
-              },
-            },
-          })
-
-          client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-        end
-        return true
-      end,
-    })
+    -- require("lspconfig").lua_ls.setup({
+    --   on_init = function(client)
+    --     local path = client.workspace_folders[1].name
+    --     if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+    --       client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
+    --         Lua = {
+    --           diagnostics = {
+    --             globals = { "vim", "bufnr" },
+    --           },
+    --           runtime = {
+    --             -- Tell the language server which version of Lua you're using
+    --             -- (most likely LuaJIT in the case of Neovim)
+    --             version = "LuaJIT",
+    --           },
+    --           -- Make the server aware of Neovim runtime files
+    --           workspace = {
+    --             checkThirdParty = false,
+    --             library = {
+    --               vim.env.VIMRUNTIME,
+    --               -- "${3rd}/luv/library"
+    --               -- "${3rd}/busted/library",
+    --             },
+    --             -- or pull in all of "runtimepath". NOTE: this is a lot slower
+    --             -- library = vim.api.nvim_get_runtime_file("", true)
+    --           },
+    --         },
+    --       })
+    --
+    --       client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    --     end
+    --     return true
+    --   end,
+    -- })
   end,
 }
