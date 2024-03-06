d2ee50aa45e2fc0e47a25988ac5e36b3152be05a
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-03-06 06:27:44 -0800

    Chore: Code Cleanup

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -14,6 +14,7 @@ return {
 
     require("mason").setup({})
     require("mason-update-all").setup({})
+
     require("mason-null-ls").setup({
       ensure_installed = {
         -- Opt to list sources here, when available in mason.
@@ -92,9 +93,9 @@ return {
                 workspace = {
                   checkThirdParty = false,
                   library = {
-                    vim.env.VIMRUNTIME,
                     "${3rd}/luv/library",
-                    -- unpack(vim.api.nvim_get_runtime_file("", true)),
+                    -- or pull in all of "runtimepath". NOTE: this is a lot slower
+                    vim.env.VIMRUNTIME,
                     -- "${3rd}/busted/library",
                   },
                 },
@@ -104,40 +105,5 @@ return {
         end,
       },
     })
-
-    -- Servers that are not installed by mason.
-    -- require("lspconfig").lua_ls.setup({
-    --   on_init = function(client)
-    --     local path = client.workspace_folders[1].name
-    --     if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-    --       client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-    --         Lua = {
-    --           diagnostics = {
-    --             globals = { "vim", "bufnr" },
-    --           },
-    --           runtime = {
-    --             -- Tell the language server which version of Lua you're using
-    --             -- (most likely LuaJIT in the case of Neovim)
-    --             version = "LuaJIT",
-    --           },
-    --           -- Make the server aware of Neovim runtime files
-    --           workspace = {
-    --             checkThirdParty = false,
-    --             library = {
-    --               vim.env.VIMRUNTIME,
-    --               -- "${3rd}/luv/library"
-    --               -- "${3rd}/busted/library",
-    --             },
-    --             -- or pull in all of "runtimepath". NOTE: this is a lot slower
-    --             -- library = vim.api.nvim_get_runtime_file("", true)
-    --           },
-    --         },
-    --       })
-    --
-    --       client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-    --     end
-    --     return true
-    --   end,
-    -- })
   end,
 }
