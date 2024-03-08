eb453f710514f1402a06983b3b97dcde3c0fb9a6
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-03-08 04:00:53 -0800

    Chore: Update Plugins

--- lua/plugins/theme/treesitter.lua
+++ lua/plugins/theme/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   "nvim-treesitter/nvim-treesitter",
-  tag = "v0.9.2",
+  -- tag = "v0.9.2",
   dependencies = {
     "nvim-treesitter/nvim-treesitter-textobjects",
     "windwp/nvim-ts-autotag",
