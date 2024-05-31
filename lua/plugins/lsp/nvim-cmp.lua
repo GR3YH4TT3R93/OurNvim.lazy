51355d08547b9d88ffbc5006d27037c91143a1f8
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-05-30 17:45:41 -0700

    Feat: Update Completion Strategy

--- lua/plugins/lsp/nvim-cmp.lua
+++ lua/plugins/lsp/nvim-cmp.lua
@@ -49,7 +49,7 @@ return {
     require("luasnip.loaders.from_vscode").lazy_load()
 
     local has_words_before = function()
-      unpack = unpack or table.unpack
+      unpack = unpack
       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
     end
@@ -96,8 +96,8 @@ return {
               fallback()
             end
           end,
-          s = cmp.mapping.confirm({ select = true }),
-          c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
+          -- s = cmp.mapping.confirm({ select = true }),
+          -- c = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.Replace, select = false }),
         }),
 
         -- Super Tab Completion
