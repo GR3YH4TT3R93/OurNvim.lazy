5d06d82930e8cd5cc355587c2159021d9ca68f19
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-06-19 15:25:24 -0700

    Feat: Update Volar Config

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -137,7 +137,7 @@ return {
                 hybridMode = false,
               },
               typescript = {
-                tsdk = vim.fn.getcwd() .. "/node_modules/typescript",
+                tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
               },
             },
             capabilities = capabilities,
