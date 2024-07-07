070ddda1a91a320a5e9ad50b4b281c1fb8655c72
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-07-06 23:03:55 -0700

    Feat: Add CopilotChat.nvim

--- lua/plugins/lsp/copilot-chat.lua
+++ lua/plugins/lsp/copilot-chat.lua
@@ -2,17 +2,20 @@ return {
   "CopilotC-Nvim/CopilotChat.nvim",
   branch = "canary",
   dependencies = {
-    { "github/copilot.vim" }, -- or github/copilot.vim
+    { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
     { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
   },
   opts = {
     debug = false, -- Enable debugging
-    window = {
-      layout = "float",
-      border = "rounded",
-    },
-    system_prompt = "",
+    show_folds = false,
+    show_help = false,
+    auto_insert_mode = true,
     -- See Configuration section for rest
+    mappings = {
+      close = {
+        normal = "<leader>x",
+        insert = "<leader>x",
+      },
+    },
   },
-  -- See Commands section for default commands if you want to lazy load on them
 }
