467891105ab4f2b1ec6a16021cc9acac8298784b
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-07-16 17:24:12 -0700

    feat: add tailwind-fold.nvim

---
+++ lua/plugins/behavior/tailwind-fold.lua
@@ -0,0 +1,7 @@
+return {
+  "razak17/tailwind-fold.nvim",
+  event = "BufRead",
+  opts = {},
+  dependencies = { "nvim-treesitter/nvim-treesitter" },
+  ft = { "html", "svelte", "astro", "vue", "typescriptreact", "php", "blade" },
+}
