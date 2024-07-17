eb94039e92ccdf6eb17ecd3756ef71505313761f
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-07-16 17:23:08 -0700

    feat: add markview.nvim

---
+++ lua/plugins/behavior/markview.lua
@@ -0,0 +1,28 @@
+return {
+  "OXY2DEV/markview.nvim",
+  ft = "markdown",
+  dependencies = {
+    -- You may not need this if you don't lazy load
+    -- Or if the parsers are in your $RUNTIMEPATH
+    "nvim-treesitter/nvim-treesitter",
+
+    "nvim-tree/nvim-web-devicons",
+  },
+  -- opts = {
+  --   code_blocks = {
+  --     style = "language",
+  --     position = "overlay",
+  --
+  --     min_width = 60,
+  --     pad_amount = 3,
+  --
+  --     language_names = {
+  --       { "cpp", "c++" },
+  --       { "py", "python" },
+  --     },
+  --
+  --     sign = true,
+  --     sign_hl = nil,
+  --   },
+  -- },
+}
