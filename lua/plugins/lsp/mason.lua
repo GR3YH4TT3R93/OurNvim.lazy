f55db3f2bec9f5dcabc8b48215ee574cff09b66e
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-07-17 03:42:20 -0700

    fix: update mason for termux

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -128,7 +128,7 @@ return {
     })
 
     require("mason-lspconfig").setup({
-      automatic_installation = true,
+      automatic_installation = { exclude = { "lua_ls" } },
       handlers = {
         -- The first entry (without a key) will be the default handler
         -- and will be called for each installed server that doesn't have
@@ -221,5 +221,41 @@ return {
     })
 
     require("dapui").setup()
+
+    -- Servers that are not installed by mason.
+    require("lspconfig").lua_ls.setup({
+      on_init = function(client)
+        local path = client.workspace_folders[1].name
+        if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+          return
+        end
+
+        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = "LuaJIT",
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              -- vim.env.VIMRUNTIME,
+              -- Depending on the usage, you might want to add additional paths here.
+              -- "${3rd}/luv/library",
+              -- "${3rd}/busted/library",
+              -- "~/.local/share/nvim/lazy",
+            },
+          },
+          hint = {
+            enable = true,
+          },
+        })
+      end,
+      settings = {
+        Lua = {},
+      },
+      capabilities = capabilities,
+    })
   end,
 }
