e0e6eaad3c281fc54736aa53e4378b559caf4610
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-07-27 11:57:46 -0700

    feat: change markdown viewer

--- lua/plugins/behavior/markview.lua
+++ lua/plugins/behavior/markview.lua
@@ -1,13 +1,13 @@
 return {
-  "OXY2DEV/markview.nvim",
-  ft = "markdown",
-  dependencies = {
-    -- You may not need this if you don't lazy load
-    -- Or if the parsers are in your $RUNTIMEPATH
-    "nvim-treesitter/nvim-treesitter",
-
-    "nvim-tree/nvim-web-devicons",
-  },
+  -- "OXY2DEV/markview.nvim",
+  -- ft = "markdown",
+  -- dependencies = {
+  --   -- You may not need this if you don't lazy load
+  --   -- Or if the parsers are in your $RUNTIMEPATH
+  --   "nvim-treesitter/nvim-treesitter",
+  --
+  --   "nvim-tree/nvim-web-devicons",
+  -- },
   -- opts = {
   --   code_blocks = {
   --     style = "language",
