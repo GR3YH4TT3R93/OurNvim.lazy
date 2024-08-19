cdbf60ce8554a1b2cf88dec1b60636336b2d0b03
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-08-19 15:28:46 -0700

    feat: reorder reqs

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+require("settings")
 require("lazy").setup({
   spec = {
     { import = "plugins.behavior" },
@@ -38,5 +39,4 @@ require("lazy").setup({
     },
   },
 })
-require("settings")
 require("keymaps")
