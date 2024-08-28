9d92089de0069604440bbba96e18fdc3040ee351
GR3YH4TT3R93 <76727386+GR3YH4TT3R93@users.noreply.github.com>
2024-08-27 21:00:23 -0700

    fix: revert telescope-frecency to working version

--- lua/plugins/telescope/frecency.lua
+++ lua/plugins/telescope/frecency.lua
@@ -1,5 +1,6 @@
 return {
   "nvim-telescope/telescope-frecency.nvim",
+  commit = "673585ee997b40d2600eb86c3693d552e4f5d79f",
   config = function()
     require("telescope").load_extension("frecency")
     vim.api.nvim_set_hl(0, "TelescopePathSeparator", {})
